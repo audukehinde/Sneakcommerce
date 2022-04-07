08ff81f4828683ec96831661994efe7be84b770e
audukehinde <auduomohkehinde@gmail.com>
2022-04-07 17:45:15 -0400

    Add modal javascript code

--- app.js
+++ app.js
@@ -123,3 +123,14 @@ currentProductSizes.forEach((size, index) => {
   })
 })
 
+const productButton = document.querySelector('.product-btn');
+const payment = document.querySelector('.payment');
+const close = document.querySelector('.close');
+
+productButton.addEventListener('click', () => {
+  payment.style.display = "flex";
+})
+
+close.addEventListener('click', () => {
+  payment.style.display = "none";
+})
