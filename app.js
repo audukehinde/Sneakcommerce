1d5e9d46bef1edb717c4762a7bca2a7b5320e6f0
audukehinde <auduomohkehinde@gmail.com>
2022-04-07 10:32:58 -0400

    Add product array

--- app.js
+++ app.js
@@ -1,9 +1,98 @@
 const wrapper = document.querySelector(".slider-wrapper");
 const menuItem = document.querySelectorAll(".menu-item");
 
+const products = [
+  {
+    id: 1,
+    title: "Air Force",
+    price: 119,
+    colors: [
+      {
+        code: "black",
+        img: "./img/air.png",
+      },
+      {
+        code: "darkblue",
+        img: "./img/air2.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: 149,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/jordan.png",
+      },
+      {
+        code: "green",
+        img: "./img/jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Blazer",
+    price: 109,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/blazer.png",
+      },
+      {
+        code: "green",
+        img: "./img/blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Crater",
+    price: 129,
+    colors: [
+      {
+        code: "black",
+        img: "./img/crater.png",
+      },
+      {
+        code: "lightgray",
+        img: "./img/crater2.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Hippie",
+    price: 99,
+    colors: [
+      {
+        code: "gray",
+        img: "./img/hippie.png",
+      },
+      {
+        code: "black",
+        img: "./img/hippie2.png",
+      },
+    ],
+  },
+];
+
+let chosenProduct = products[0];
+
+const currentProductImg = document.querySelector('.product-img');
+const currentProductTitle = document.querySelector('.product-title');
+const currentProductPrice = document.querySelector('.product-price');
+const currentProductColors = document.querySelectorAll('.color');
+const currentProductSizes = document.querySelectorAll('.size');
+
 menuItem.forEach((item, index) => {
   item.addEventListener('click', () => {
+    // Change current slide
     wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    // Change the choosen Product
   })
 })
 
